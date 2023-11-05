0cc3a416abc2c86bc3d15cda45fdb5dca361d97b
axxyush <ayushsrivastava192004@gmail.com>
2023-11-04 23:33:35 -0400

    update

---
+++ static/index.js
@@ -0,0 +1,13 @@
+document.querySelector(".helpbtn").addEventListener('click', function () {
+    window.location.href = "/help";
+})
+
+document.querySelector(".aboutbtn").addEventListener('click', function () {
+    window.location.href = "/about";
+})
+
+document.querySelector(".homebtn").addEventListener('click', function () {
+    window.location.href = "/ayush";
+})
+
+const slides = ["Saab", "Volvo", "BMW"];
--- views.py
+++ views.py
@@ -5,3 +5,15 @@ views = Blueprint(__name__, "views")
 @views.route("/")
 def home():
     return render_template('index.html')
+
+@views.route("/ayush")
+def ayush():
+    return render_template('ayush.html')
+
+@views.route("/about")
+def about():
+    return render_template('about.html')
+
+@views.route("/help")
+def help():
+    return render_template('help.html')
