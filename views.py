cad293ca26be09545e5d7972a58fc17e2a461c20
maxence-glt <maxencegilloteaux2023@gmail.com>
2023-11-04 20:12:44 -0400

    backend api

--- views.py
+++ views.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, render_template, request
+from api import weather_specs
+import datetime as dt
+
+weather_specs_default = weather_specs("Salinas Valley")
 
 views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', jsonFile=weather_specs_default)
