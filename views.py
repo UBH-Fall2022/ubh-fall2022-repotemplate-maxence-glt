b46d56462df80f1ab700d45dceb2fd255ca778f5
IsmaelVillacis <ivillacis33@gmail.com>
2023-11-05 01:37:49 -0400

    added backup

--- views.py
+++ views.py
@@ -1,7 +1,23 @@
 from flask import Blueprint, render_template, request
+from api import weather_specs
+
+defaultWeather = weather_specs("Buffalo")
+locationIndex = "Buffalo"
 
 views = Blueprint(__name__, "views")
 
-@views.route("/")
+@views.route("/", methods = ["POST", "GET"])
 def home():
-    return render_template('index.html')
+    global defaultWeather
+    if request.method == "GET":
+        return render_template('index.html', loc=locationIndex, test=defaultWeather)
+    
+    if request.method == "POST":
+        location = request.form.get("location")
+        defaultWeather = weather_specs(location)
+        locationIndex=location
+
+        if location == None:
+            return render_template("error.html", request=location)
+
+        return render_template("index.html", loc=locationIndex, test=defaultWeather)
